1528e3ca04d5d9fca30978423a1c866c6b3dd91a
Olbeck80920 <oliviabeck80920@gmail.com>
2019-10-16 09:17:19 -0600

    Added function files and simulated data.

---
+++ R/ANSIM.R
@@ -0,0 +1,10 @@
+#' All Nightly Operations Data Simulated
+#'
+#' All Data points from fy14-fy20
+#'
+#' @format a data frame
+#' \describe{
+#'   \item{Rides}{Numebr of Rides}
+#'   }
+#' @source All data comes from CSU safe ride progam
+"ANSIM"
